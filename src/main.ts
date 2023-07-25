e62dd8310033c779d2215ae470f5acdc8e0d5177
lanye74 <layne.ogden727@gmail.com>
2023-07-25 14:03:06 -0400

    implement trimFiggedText

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ const input = document.getElementById("input") as HTMLInputElement;
 const output = document.getElementById("output") as HTMLTextAreaElement;
 
 
+// TODO: rewrite Figlet myself, to make it more efficient and in typescript
 // @ts-expect-error
 let figlet = new Figlet() as Figlet;
 
@@ -12,12 +13,27 @@ await fetch("./assets/Doom.flf")
 
 
 
-input.addEventListener("keyup", async (_) => {
+input.addEventListener("keyup", () => {
 	let figgedText = figlet.getText(input.value);
 
+	const figgedLength = figgedText.split("\n")[0].length;
+	const stars = "*".repeat(figgedLength);
+
+	figgedText = `${stars}\n${trimFiggedText(figgedText)}\n${stars}`;
 
 	output.textContent = figgedText;
 });
+
+
+
+function trimFiggedText(text: string): string {
+	let lines = text.split("\n");
+
+	return lines
+		.map(line => line.trimEnd())
+		.filter(line => line !== "")
+		.join("\n")
+}
 
 
 
